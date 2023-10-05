5ceaadf07dcac3e8fa9d1d18a20c6d369ceaf2c3
Shivang Mishra <shivang978@gmail.com>
2023-10-05 17:16:26 +0530

    navigation screen

--- screens/Navigation.jsx
+++ screens/Navigation.jsx
@@ -2,6 +2,7 @@ import { View, Text, Button, StyleSheet } from 'react-native'
 import SignIn from './SignIn'
 import Register from './Register'
 import AccountDetails from './AccountDetails'
+import HomePage from './HomePage'
 
 
 const Navigation = (props) => {
@@ -11,6 +12,7 @@ const Navigation = (props) => {
     <Button title='Register Screen' onPress={()=>{props.navigation.navigate(Register)}}  />
     <Button title='Sign in Screen' onPress={()=>{props.navigation.navigate(SignIn)}}  />
     <Button title='Account Details Screen' onPress={()=>{props.navigation.navigate(AccountDetails)}}  />
+    <Button title='Home Page' onPress={()=>{props.navigation.navigate(HomePage)}}  />
     
     </View>
   )
